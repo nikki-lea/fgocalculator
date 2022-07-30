bcd6919536a18bfb4adb61ff561b75a2ef33ee1d
Nicole <nicoleleabauer@gmail.com>
2022-07-29 18:05:16 -0700

    Initial sizing and positioning of layout

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -8,10 +8,15 @@ const Layout: React.FC<{children: React.ReactNode}> = ({children}) => {
         <title>Fate Grand Order Probability Calculator</title>
         <meta name="description" content="Calculator for Fate Grand Order roll probabilities" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat" rel="stylesheet" />
       </Head>
-      <Image height={1078} width={2133} src="/67BrG93.jpeg" />
-        <h1>Fate Grand Order Probability Calculator</h1>
-          <main>{children}</main>
+      <div className="main-container">
+        <div className="hero-image">
+          <h1 className="header">Fate Grand Order Probability Calculator</h1>
+          <Image height={1078} width={2133} layout="responsive" src="/67BrG93.jpeg" />
+        </div>
+        <main>{children}</main>
+      </div>
       </>
     )
   }
