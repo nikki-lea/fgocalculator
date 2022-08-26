9cae7559adae2611213113adc29c3ad27e7e84d4
Nicole <nicoleleabauer@gmail.com>
2022-08-25 17:32:18 -0700

    Done with logic omg

--- pages/probability.tsx
+++ pages/probability.tsx
@@ -1,23 +1,30 @@
 import { NextPage } from "next";
 import Image from "next/image";
 import { useTranslation } from "next-i18next";
-import { useContext, useRef, useState } from "react";
+import { useContext, useState } from "react";
 import { FgoContext, TargetOptions } from "../contexts";
 import { Slider } from "@mui/material";
+import { TargetDataType } from "../types/contexts";
+import calcProbability from "../utils/calcProbability";
 
 const Probability: NextPage = () => {
   const { t } = useTranslation();
   const { state } = useContext(FgoContext);
   const { totalSQForBanner, targetData } = state;
   const startingBudget = totalSQForBanner / targetData.length;
-  const initialProbability = 0;
-  const [targetSQ, setTargetSQ] = useState<number[]>(Array(3).fill(startingBudget));
-  const [probabilities, setProbabilities] = useState<number[]>(Array(3).fill(initialProbability));
+  const initialProbability: number[] = [];
+  targetData.forEach((item) => initialProbability.push(calcProbability({...item, sq: startingBudget})));
+  const [targetSQ, setTargetSQ] = useState<number[]>(Array(targetData.length).fill(startingBudget));
+  const [probabilities, setProbabilities] = useState<number[]>(initialProbability);
 
-  const handleChange = (index: number) => (_event: Event, newValue: number | number[]) => {
+  const handleChange = (index: number, targetData: TargetDataType) => (_event: Event, newValue: number | number[]) => {
     const targetCopy = [...targetSQ];
-    targetSQ[index] = newValue as number; // We will always be passing a number to state here
+    const SQUsed = newValue as number;
+    targetCopy[index] = SQUsed; // We will always be passing a number to state here
     setTargetSQ(targetCopy);
+    const newProbabilities = [...probabilities];
+    newProbabilities[index] = calcProbability({...targetData, sq: SQUsed});
+    setProbabilities(newProbabilities);
   };
 
   return (
@@ -32,7 +39,7 @@ const Probability: NextPage = () => {
           height={36}
           width={36}
         />
-        <span>{state.totalSQForBanner}</span>
+        <span>{totalSQForBanner.toLocaleString('en-US')}</span>
       </div>
       <div className="probability-calculations">
         {
@@ -44,14 +51,20 @@ const Probability: NextPage = () => {
               <h3>{`${item.rarity}* ${typeCopy}`}</h3>
               <Slider
                 aria-label="calculatedprobability"
-                value={targetSQ[index]}
-                onChange={handleChange(index)}
+                defaultValue={startingBudget}
+                onChange={handleChange(index, item)}
+                valueLabelDisplay="on"
+                max={totalSQForBanner}
               />
               <h3>{t("rateup.chance.probability")}</h3>
               <h3>{probabilities[index]}</h3>
             </>)
             })
         }
+        <div className="are-you-whale">
+          <h1>{targetSQ.length && targetSQ.reduce((acc, current) => (acc + current), 0)}</h1>
+          <h3>{targetSQ.length &&  totalSQForBanner - (targetSQ.reduce((acc, current) => (acc + current)))}</h3>
+        </div>
       </div>
     </div>
   );
---
+++ utils/calcProbability.tsx
@@ -0,0 +1,23 @@
+import moment from "moment";
+import { JPEventPropsType } from "../types/utils/calcJPEventSQ";
+import EventSQData from "../data/events";
+import { TargetDataType } from "../types/contexts";
+import { TargetOptions } from "../contexts";
+
+// The index of the array indicates the rarity of the target, i.e. fgoProbabilities.SERVANT[5] = 0.008
+const fgoProbabilities = {
+  CE: [0,0,0,0.08,0.04,0.028],
+  SERVANT: [0,0,0,0.04,0.015,0.008]
+};
+
+
+const calcProbability = ({ sq, type, rarity, shared = 1 }: TargetDataType & {sq: number}): number => {
+  const summonProbability = fgoProbabilities[type as keyof typeof fgoProbabilities][rarity]/shared;
+  const rolls = sq/3;
+  const exponent = (rolls+(rolls/10));
+  const result = 1-Math.pow((1-summonProbability), exponent);
+  console.log(`Type ${type} rarity ${rarity} shared ${shared}, sq ${sq} exponent ${exponent} probability ${summonProbability} result ${result}`)
+  return result;
+};
+
+export default calcProbability;
